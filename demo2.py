ac5d0b01da5e2a6e4a8fe4fcb3e33af29db5fa38
Lordera1209 <songzy1209@gmail.com>
2024-07-23 10:12:04 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -8,14 +8,6 @@
 
 import pandas as pd
 import streamlit as st
-from ydata_profiling import ProfileReport
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
+
 
 st.header("资料信息分析")
-
-df = pd.read_csv('.data/data.csv')
-
-report = ProfileReport(df, title='My Data')
-report.to_file("my_report.html")
-st_profile_report(report)
